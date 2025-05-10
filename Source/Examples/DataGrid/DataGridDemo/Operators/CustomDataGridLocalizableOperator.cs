3633c68644a857b6ea4c79d927436b7a73487ad7
Andrii <aprotskiv@gmail.com>
2025-05-10 17:06:06 +0700

    added DataGrid example
    use custom localizable operator in ObservableCollectionOfExampleObjectTranslatedExample.xaml

---
+++ Source/Examples/DataGrid/DataGridDemo/Operators/CustomDataGridLocalizableOperator.cs
@@ -0,0 +1,35 @@
+﻿using DataGridDemo.Resources;
+using PropertyTools.Wpf.Operators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridDemo.Operators
+{
+    public class CustomDataGridLocalizableOperator : DefaultLocalizableOperator
+    {
+        public override string GetLocalizedString(string key, Type declaringType)
+        {
+            var value = key;
+
+            var resourceKey = key;
+            if (resourceKey != null)
+            {
+                if (declaringType != null)
+                {
+                    resourceKey = declaringType.FullName + "." + resourceKey;
+                }
+
+                var resourceValue = Translations.ResourceManager.GetString(resourceKey);
+                if (resourceValue != null)
+                {
+                    value = resourceValue;
+                }
+            }
+
+            return value;
+        }
+    }   
+}
