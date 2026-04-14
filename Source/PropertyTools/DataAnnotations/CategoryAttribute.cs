9322712adaa735e4fa856ae8634b4cecd84f6190
Andrii Protskiv <aprotskiv@gmail.com>
2026-04-14 11:29:34 +0700

    introduce category (tab) sort index

--- Source/PropertyTools/DataAnnotations/CategoryAttribute.cs
+++ Source/PropertyTools/DataAnnotations/CategoryAttribute.cs
@@ -9,27 +9,44 @@
 
 namespace PropertyTools.DataAnnotations
 {
-    using System;
+	using System;
 
-    /// <summary>
-    /// Specifies the name of the category in which to group the property or event when displayed in a PropertyGrid control.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class CategoryAttribute : AbstractAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CategoryAttribute"/> class.
-        /// </summary>
-        /// <param name="category">The category.</param>
-        public CategoryAttribute(string category)
-        {
-            this.Category = category;
-        }
+	/// <summary>
+	/// Specifies the name of the category in which to group the property or event when displayed in a PropertyGrid control.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Property)]
+	public class CategoryAttribute : AbstractAttribute
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CategoryAttribute"/> class.
+		/// </summary>
+		/// <param name="category">The category.</param>
+		public CategoryAttribute(string category)
+		{
+			this.Category = category;
+		}
 
-        /// <summary>
-        /// Gets the category.
-        /// </summary>
-        /// <value>The category.</value>
-        public virtual string Category { get; private set; }
-    }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CategoryAttribute"/> class.
+		/// </summary>
+		/// <param name="category">The category.</param>
+		/// <param name="sortIndex">The category sort index.</param>
+		public CategoryAttribute(string category, uint sortIndex)
+		{
+			this.Category = category;
+			this.SortIndex = sortIndex;
+		}
+
+		/// <summary>
+		/// Gets the category.
+		/// </summary>
+		/// <value>The category.</value>
+		public virtual string Category { get; private set; }
+
+		/// <summary>
+		/// Gets the category sort index
+		/// </summary>
+		/// <value>The category sort index.</value>
+		public virtual uint? SortIndex { get; private set; }
+	}
 }
